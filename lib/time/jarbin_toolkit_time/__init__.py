3ec231746e0b5914109975931ab55c8bfcc829a9
NathanAMARAGGI <namaraggi@snapp.fr>
2026-09-22 10:40:55 +0200

    [UPDATE] starting refactor

--- lib/time/jarbin_toolkit_time/__init__.py
+++ lib/time/jarbin_toolkit_time/__init__.py
@@ -1,40 +1,18 @@
-#############################
-###                       ###
-###     Jarbin-ToolKit    ###
-###         time          ###
-###  ----__init__.py----  ###
-###                       ###
-###=======================###
-### by JARJARBIN's STUDIO ###
-#############################
-
-
-from jarbin_toolkit_time.time import Time
-from jarbin_toolkit_time.stopwatch import StopWatch
-
-
-def get_timestamp(
-    ) -> float:
-    """
-        Get the current timestamp
-
-        Returns:
-            float : timestamp
-    """
-
-    from time import monotonic
-
-    return monotonic()
-
-
-__all__ : list[str] = [
-    'get_timestamp',
-    'Time',
-    'StopWatch'
-]
+# ============================================================================
+# JARBIN-TOOLKIT
+#
+# Package      : Time
+# File         : __init__.py
+#
+# Author       : Jarjarbin06
+# ============================================================================
 
 
 __author__ : str = 'Nathan Jarjarbin'
 __email__ : str = 'nathan.amaraggi@epitech.eu'
-__version__ : str = "0.1.2.2"
+__version__ : str = "1.0.0.0"
 __license__ : str = "GPL"
+
+
+__all__ : list[str] = [
+]
